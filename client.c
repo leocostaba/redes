194b24b7842288a5d75a8dbb84ef54ccd17ba770
Jonathan Queiroz <dev@johnjq.com>
2017-03-08 16:40:07 -0300

    Slightly improve prototypes of client and server.

--- client.c
+++ client.c
@@ -8,7 +8,9 @@ void on_init(Connection* const conn) {
 
 void on_receive(Connection* const conn, const uint8_t* const payload, const size_t bytes) {
     printf("(client) Received %u bytes: ", bytes);
-    fwrite(payload, 1, bytes, stdin);
+    for (int i = 0; i < bytes; ++i)
+        putchar((char) payload[i]);
+    puts("");
 }
 
 int main() {
--- server.c
+++ server.c
@@ -10,6 +10,8 @@ void on_receive(Connection* const conn, const uint8_t* const payload, const size
     for (int i = 0; i < bytes; ++i)
         putchar((char) payload[i]);
     puts("");
+    printf("(server) Sending a response\n");
+    send_message(conn, "recebi", 6);
     /*printf("x = %d\n", (int) *payload);*/
 }
 
