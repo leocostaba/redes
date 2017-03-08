73327818e141a5c53257facb71546a0b171c5a0e
Jonathan Queiroz <dev@johnjq.com>
2017-03-08 18:01:32 -0300

    Start writing the application layer.

--- client.c
+++ client.c
@@ -1,20 +1,79 @@
 #include "transport.h"
 #include <stdio.h>
+#include <string.h>
+#define MAX_REMOTE_FILENAME_LEN 200
+#define MAX_STATUS_MESSAGE_LEN 200
 //TODO: add thread
+
+FILE* file;
+uint8_t initial_message[MAX_REMOTE_FILENAME_LEN+7];
 
 void on_init(Connection* const conn) {
     puts("(client) Connection initialized");
-    send_message(conn, "olamundo", 5);
+    send_message(conn, initial_message, MAX_REMOTE_FILENAME_LEN+7);
 }
 
+//TODO: improve the transport layer (fixed number of expected bytes)
+uint8_t first_message = 1;
 void on_receive(Connection* const conn, const uint8_t* const payload, const size_t bytes) {
-    printf("(client) Received %u bytes: ", bytes);
-    for (int i = 0; i < bytes; ++i)
-        putchar((char) payload[i]);
-    puts("");
+    puts("asd");
+    if (first_message) {
+        first_message = 0;
+        const uint32_t status_code = read_uint32(payload);
+        char status_message[MAX_STATUS_MESSAGE_LEN+1];
+        memcpy(status_message, payload+4, MAX_STATUS_MESSAGE_LEN);
+        printf("(client) Status code: %d\n", (int) status_code);
+        printf("(client) Status message: %s\n", status_message);
+        if (status_code != 200) {
+            puts("(client) Exiting...");
+            //TODO: finish connection
+            return;
+        }
+    } else {
+        printf("(client) Received %u bytes\n", bytes);
+        fwrite(payload, 1, bytes, file);
+    }
 }
 
-int main() {
+int run_get(const char* const local_filename, const char* const remote_filename) {
+    const size_t remote_filename_len = strlen(remote_filename);
+    if (remote_filename_len > MAX_REMOTE_FILENAME_LEN) {
+        printf("remote filename is too long: %s\n", remote_filename);
+        return 1;
+    }
+    file = fopen(local_filename, "w");
+    if (file == 0) {
+        printf("unable to open file: %s\n", local_filename);
+        return 1;
+    }
+    initial_message[0] = 'G';
+    initial_message[1] = 'E';
+    initial_message[2] = 'T';
+    memcpy(initial_message+3, remote_filename, remote_filename_len);
+    initial_message[3+remote_filename_len] = '\0';
+    const int ret = start_client(on_init, on_receive);
+    return ret;
+}
+
+int run_put(const char* const local_filename, const char* const remote_filename) {
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 4) {
+        printf("expected 3 arguments, received %d\n", argc-1);
+        return 1;
+    }
+    if (strcmp(argv[1], "GET") == 0) {
+        return run_get(argv[2], argv[3]);
+    } else if (strcmp(argv[1], "PUT") == 0) {
+        return run_put(argv[2], argv[3]);
+    } else {
+        printf("invalid method %s, expected GET or PUT\n", argv[1]);
+        return -1;
+    }
+
+    return 0;
     const int ret = start_client(on_init, on_receive);
     printf("(client) Connection closed: %d\n", ret);
     return 0;
--- server.c
+++ server.c
@@ -1,18 +1,45 @@
 #include "transport.h"
 #include <stdio.h>
+#define MAX_REMOTE_FILENAME_LEN 200
+#define MAX_STATUS_MESSAGE_LEN 200
+#define BUFFER_SIZE 4096
 
 void on_init(Connection* const conn) {
     puts("(server) Connection initialized");
 }
 
 void on_receive(Connection* const conn, const uint8_t* const payload, const size_t bytes) {
-    printf("(server) Received %u bytes: ", bytes);
-    for (int i = 0; i < bytes; ++i)
-        putchar((char) payload[i]);
-    puts("");
-    printf("(server) Sending a response\n");
-    send_message(conn, "recebi", 6);
-    /*printf("x = %d\n", (int) *payload);*/
+    if (payload[0] == 'G' && payload[1] == 'E' && payload[2] == 'T') {
+        char filename[MAX_REMOTE_FILENAME_LEN+1];
+        memcpy(filename, payload+3, MAX_REMOTE_FILENAME_LEN);
+        FILE* file = fopen(filename, "r");
+        if (file == 0) {
+            char response[MAX_STATUS_MESSAGE_LEN+4];
+            write_uint32(response, 404);
+            strcpy(response+4, "Not Found");
+            send_message(conn, response, MAX_STATUS_MESSAGE_LEN+4);
+            //TODO: terminate connection
+            return;
+        }
+        char response[MAX_STATUS_MESSAGE_LEN+4];
+        write_uint32(response, 200);
+        strcpy(response+4, "OK");
+        send_message(conn, response, MAX_STATUS_MESSAGE_LEN+4);
+        char buffer[BUFFER_SIZE];
+        for (;;) {
+            size_t bread = fread(buffer, 1, BUFFER_SIZE, file);
+            if (bread == 0) {
+                //TODO: terminate connection
+                return;
+            }
+            send_message(conn, buffer, bread);
+            //TODO: wait if the "send" fails
+        }
+    } else if (payload[0] == 'P' && payload[1] == 'U' && payload[2] == 'T') {
+        puts("rec put");
+    } else {
+        puts("WARNING: invalid method");
+    }
 }
 
 int main() {
