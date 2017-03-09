3e6affb08bae65cf1c4593c32736cbd7cc473fad
leocostaba <leonardocosta@infojr.com.br>
2017-03-09 09:15:25 -0300

    Sending PUT (cliente and server) teste with real files)

--- server.c
+++ server.c
@@ -49,7 +49,45 @@ void run_get(Connection* const conn, const uint8_t* const message) {
 }
 
 void run_put(Connection* const conn, const uint8_t* const message) {
-    //TODO: implement this function
+    // Open the file for read access
+    char filename[MAX_LOCAL_FILENAME_LEN+1];
+    memcpy(filename, message+3, MAX_LOCAL_FILENAME_LEN);
+    printf("(server) Opening file for read access: %s\n", filename);
+    FILE* file = fopen(filename, "r");
+    // If it fails, reply with a failure message and terminate the connection
+    if (file == 0) {
+        puts("(server) Unable to open the file: replying with failure");
+        uint8_t response[MAX_STATUS_MESSAGE_LEN+8];
+        write_uint32(response, 404);
+        strcpy((char*) response+4, "Not Found");
+        send_message_blocking(conn, response, MAX_STATUS_MESSAGE_LEN+8);
+        puts("(server) Terminating the connection");
+        terminate_connection(conn);
+        return;
+    }
+    // If it succeeds, reply with a success message containing the file length
+    fseek(file, 0, SEEK_END);
+    const uint32_t file_length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    uint8_t response[MAX_STATUS_MESSAGE_LEN+8];
+    puts("(server) Success opening the file: replying with the file length");
+    write_uint32(response, 200);
+    strcpy((char*) response+4, "OK");
+    write_uint32(response+204, file_length);
+    send_message_blocking(conn, response, MAX_STATUS_MESSAGE_LEN+8);
+    // And then send the file contents
+    puts("(server) Geting file contents...");
+    uint8_t buffer[BUFFER_SIZE];
+    for (;;) {
+        size_t bread = fread(buffer, 1, BUFFER_SIZE, file);
+        if (bread == 0) {
+            puts("(server) Get the last block, terminating the connection...");
+            terminate_connection(conn);
+            return;
+        }
+        puts("(server) Geting a new block");
+        send_message_blocking(conn, buffer, bread);
+    }
 }
 
 int main() {
